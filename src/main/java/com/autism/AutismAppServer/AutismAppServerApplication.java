2c9c43e3f3da4615247462a846e93aa146f87f9b
Akshay Gupta <akshay.gupta@websym.com>
2018-08-01 18:41:59 +0530

    Initial commit

---
+++ src/main/java/com/autism/AutismAppServer/AutismAppServerApplication.java
@@ -0,0 +1,45 @@
+package com.autism.AutismAppServer;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableScheduling
+public class AutismAppServerApplication extends SpringBootServletInitializer {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.autism.AutismAppServer")).paths(getPaths()).build();
+	}
+
+	@SuppressWarnings("unchecked")
+	private Predicate<String> getPaths() {
+		return or(regex("/organization.*"));
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutismAppServerApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutismAppServerApplication.class, args);
+	}
+
+}
